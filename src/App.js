b26e55a0c06919656cc2853228c863a95c81e1f6
razvanr91 <razvan.roman91@outlook.com>
2021-11-28 14:20:41 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 function App() {
   const [weather, setWeather] = useState(null);
+  const [input, setInput] = useState("");
   useEffect(() => {
     axios.get("http://api.weatherapi.com/v1/current.json?key=6c8713c06eaf468d825135331212811&q=London&aqi=yes")
       .then(data => {
@@ -13,14 +14,35 @@ function App() {
         console.log(error);
       })
   }, [])
+
+  const weatherInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const searchWeather = () => {
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=6c8713c06eaf468d825135331212811&q=${input}&aqi=yes`)
+      .then(data => {
+        setWeather(data.data)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   return (
     <div className="App">
       {weather && (
         <div>
-          <img src={weather.current.condition.icon} />
-          <h1>{weather.location.name}</h1>
-          <h2>{weather.location.region}</h2>
-          <h3>{weather.current.temp_c}&#8451;</h3>
+          <div>
+            <input onChange={weatherInput} type="texg" />
+            <button onClick={searchWeather}>Search</button>
+          </div>
+          <div>
+            <img src={weather.current.condition.icon} />
+            <h1>{weather.location.name}</h1>
+            <h2>{weather.location.region}</h2>
+            <h3>{weather.current.temp_c}&#8451;</h3>
+          </div>
         </div>
       )}
     </div>
